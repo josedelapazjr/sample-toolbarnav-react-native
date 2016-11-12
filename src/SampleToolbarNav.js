112ed5cb6c0c6954a2a1cb05f008d90ac0d37462
Joe <jwataru@gmail.com>
2016-11-12 23:11:07 +1100

    Add Home and Settings components.

--- src/SampleToolbarNav.js
+++ src/SampleToolbarNav.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import {
-    AppRegistry,
-    View,
+  AppRegistry,
+  Text,
+  View,
+  Navigator,
+  BackAndroid
 } from 'react-native';
 
+import Home from './Home';
+import Settings from './Settings';
+
+var _navigator;
+
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+    _navigator.pop();
+    return true;
+  }
+  return false;
+});
+
 class SampleToolbarNav extends Component {
+
+  _renderScene (route, navigator) {
+    switch (route.id) {
+      case 'home':
+        return (
+          <Home navigator={navigator}/>
+        )
+      case 'settings':
+        return (
+          <Settings navigator={navigator} />
+        )
+    }
+  }
+
   render() {
-          return (
-              <View></View>
-          );
+    return (
+      <Navigator
+        initialRoute={{ id: 'home' }}
+        renderScene={(route, navigator) => this._renderScene(route, navigator)}
+      />
+    );
   }
 }
 
