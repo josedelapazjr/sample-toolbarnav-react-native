306ec66871d6434d99ce6da4b60c744bec2b04db
Joe <jwataru@gmail.com>
2016-11-12 22:25:17 +1100

    Initial commit.

---
+++ index.android.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import React, { AppRegistry } from 'react-native';
+import SampleToolbarNav from './src/SampleToolbarNav';
+
+AppRegistry.registerComponent('SampleToolbarNav', () => SampleToolbarNav);
---
+++ src/SampleToolbarNav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    View,
+} from 'react-native';
+
+class SampleToolbarNav extends Component {
+  render() {
+          return (
+              <View></View>
+          );
+  }
+}
+
+export default SampleToolbarNav;
